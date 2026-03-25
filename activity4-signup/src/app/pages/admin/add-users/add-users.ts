d0620b005217d48a64af5e95927fa9afb3678dac
cher-wyn23 <cherwyn.malquisto@evsu.edu.ph>
2026-03-25 12:04:37 +0800

    Activity 4 - Signup

---
+++ activity4-signup/src/app/pages/admin/add-users/add-users.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-users.html',
+  styleUrl: './add-users.css',
+})
+export class AddUsers {
+
+}
